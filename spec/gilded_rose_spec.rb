a81dcc648ce7050978c40efc8c18d35daa58c3af
Gadiza Zerari <gadizaz@hotmail.com>
2018-05-24 13:19:50 +0100

    quality 0 when 0 days left backstage passes test

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -42,6 +42,13 @@ describe GildedRose do
       expect(items[0].quality).to eq 13
     end
 
+    it 'quality Backstage passes drops to 0 when 0 days left' do
+      items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 10)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 0
+    end
+
+
     it 'Sulfuras never decreases in quality' do
       items = [Item.new("Sulfuras, Hand of Ragnaros", 1, 40)]
       GildedRose.new(items).update_quality()
