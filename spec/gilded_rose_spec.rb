a76d73291d7fb99bef4a3e5e02a5d0a2c3ef4475
Gadiza Zerari <gadizaz@hotmail.com>
2018-05-24 11:36:02 +0100

    tests for quality Brie increasing and quality never under 0

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -8,6 +8,17 @@ describe GildedRose do
       GildedRose.new(items).update_quality()
       expect(items[0].name).to eq "foo"
     end
+    it "the quality of an item is never negative" do
+      items = [Item.new("foo", 0, 0)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 0
+
+    end
+    it "'Aged Brie' increases in quality the older it gets" do
+      items = [Item.new('Aged Brie', 0, 0)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to be > 0
+    end
   end
 
 end
